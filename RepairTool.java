4ce905158f785291217637500cdf8d60b7abd552
Charles Wilson <charliefooligan@gmail.com>
2021-04-25 11:20:10 -0400

    added to RepairTool.java

--- RepairTool.java
+++ RepairTool.java
@@ -3,12 +3,18 @@ import java.util.HashMap;
 
 public class RepairTool {
     public static void main(String[] args) {
+
+        // parse java files into simplified AST objects
         Parser parser = new Parser();
         Class instructorSimplifiedAST = parser.getSimplifiedASTForJavaFileLocatedAt("/Users/charliewilson/Desktop/ASTTestJavaFiles/Instructor1.java");
-        System.out.println(instructorSimplifiedAST);
         Class studentSimplifiedAST = parser.getSimplifiedASTForJavaFileLocatedAt("/Users/charliewilson/Desktop/ASTTestJavaFiles/Student1.java");
-        System.out.println(studentSimplifiedAST);
-        compareSubmissions(instructorSimplifiedAST, studentSimplifiedAST);
+
+        // print simple AST objects
+        System.out.println("Instructor Simplified AST: \n\n" + instructorSimplifiedAST);
+        System.out.println("Student Simplified AST: \n\n" + studentSimplifiedAST);
+
+        // compare simple ASTs for student and instructor
+        HashMap<String, Integer> differences= compareSubmissions(instructorSimplifiedAST, studentSimplifiedAST);
     }
 
     static HashMap<String, Integer> compareSubmissions(Class instructorAST, Class studentAST) {
@@ -35,7 +41,59 @@ public class RepairTool {
             differences.put("Number of field declarations does not match", 0);
         }
 
-        // compare method signatures
+        // check that student has all the method signatures that instructor has
+        ArrayList<Method> instructorMethods = instructorAST.getMethods();
+        ArrayList<Method> studentMethods = studentAST.getMethods();
+        for (int i = 0; i < instructorMethods.size(); i++) {
+            Method instructorMethod = instructorMethods.get(i);
+            String instructorMethodName = (String)instructorMethod.getName();
+            String instructorMethodReturnType = (String)instructorMethod.getReturnType();
+            ArrayList<Parameter> instructorParams = instructorMethod.getParameters();
+
+            // search for matching student method
+            boolean studentHasMethodSignature = false;
+            for (int j = 0; j < studentMethods.size(); j++) {
+
+                boolean studentHasMethodName = false;
+                boolean studentHasMethodReturnType = false;
+                boolean studentHasMethodParams = false;
+
+                Method studentMethod = studentMethods.get(j);
+                String studentMethodName = (String)studentMethod.getName();
+                String studentMethodReturnType = (String)studentMethod.getReturnType();
+                ArrayList<Parameter> studentParams = studentMethod.getParameters();
+
+                // check for matching method name
+                if (studentMethodName.equals(instructorMethodName)) {
+                    studentHasMethodName = true;
+                }
+
+                // check for matching return type
+                if (studentMethodReturnType.equals(instructorMethodReturnType)) {
+                    studentHasMethodReturnType = true;
+                }
+
+                // TODO: check for matching params
+
+                // TODO: add check for params to boolean evaluation of method signature
+                if (studentHasMethodName && studentHasMethodReturnType) {
+                    studentHasMethodSignature = true;
+                    break;
+                }
+
+
+            }
+
+            if (!studentHasMethodSignature) {
+                String difference = String.format("Instructor defined method with signature: '%s %s(...)', but you didn't define this method.\n", ((String) instructorMethod.getReturnType()).toLowerCase(), instructorMethod.getName());
+                differences.put(difference, 0);
+            }
+
+        }
+
+        // check that student methods signatures have been defined by the instructor
+
+
         // compare method instance variables
         // compare field definitions
 
